a6068eb78706f6e3e56d2314859187c119ac989f
Dhaval <dhavalkathiriya071@gmail.com>
2022-10-14 14:07:45 +0530

    first commit

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+
+$username ="root";
+$password ="";
+$server ='localhost';
+$db ='user';
+
+$con = mysqli_connect($server,$username,$password,$db);
+
+if($con){
+   ?>
+<script>
+    alert('connection successful')
+</script>
+   <?php
+}else{
+    echo "No connection";
+}
+
+?>
